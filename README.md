544ae5271156e80971dc20cf3176d1ea3c759b2f
guilherme-cota-efi <121238882+guilherme-cota-efi@users.noreply.github.com>
2022-12-23 16:48:08 -0300

    🎉Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+<h1 align="center">Efí Pay no Discord</h1>
+
+![Capa reposirório da comunidade Efí no Discord](./assets/img/thumb-repository.png)
+
+Se você tem a necessidade de integrar seu sistema ou aplicação a uma API completa de pagamentos, desejos de trocar experiências e compartilhar seu conhecimento, conecte-se à comunidade da Efí no Discord.
+
+## Sumário
+
+- [O que é](#o-que-é)
+- [Como participar](#como-participar)
+- [Comportamento](#comportamento)
+- [Efí](#efí-pay)
+
+## O que é
+
+O servidor da Efí no Discord foi criado para aproximar desenvolvedores e interessados em integrar com a Efí Pay, dar suporte e promover nossa API Pix e API cobranças. O diálogo por aqui é mais rápido e dinâmico, o que permite uma abordagem mais informal e personalizada. Além disso, você pode usar o canal para tirar dúvidas com outros integradores, tornando esse espaço uma sólida rede de apoio.
+
+Para evitar descontentamentos e um ambiente prejudicial, criamos algumas regrinhas simples para as interações realizadas. Você encontra estas regras entrando em nosso servidor. Afinal, queremos que esse seja um ambiente seguro, íntegro e aberto a trocas de experiências.
+
+## Como participar
+
+Para acessar a nossa comunidade, basta entrar no nosso [servidor](https://discord.gg/ptGHMtczcV).
+Lembrando que para isso, é necessário que você crie uma [conta no Discord](https://discord.com/register).
+
+Com o intuito de separar por tópicos os assuntos abordados, criamos grupos específicos para facilitar a comunicação. Para ajudar, listamos abaixo alguns dos canais disponíveis:
+
+![Canais da comunidade Efí no Discord](./assets/img/canais-discord.png)
+
+## Comportamento
+
+O Discord Efí não é um lugar para insultar, antagonizar ou sexualizar pessoas. Seja gentil e respeitoso em suas publicações e comentários, mesmo quando houver divergências de opinião na discussão. Não são aceitos, em nenhuma circunstância, conteúdos e comentários preconceituosos, racistas, homofóbicos e/ou ofensivos de qualquer natureza. Aqui também não é um espaço para comentários sobre aspectos físicos de participantes do grupo e colaboradores da Efí.
+
+Nesse sentido, o que esperamos de você enquanto membro ativo da comunidade, é poder tirar dúvidas sobre as APIs Efí Pay e engajar com outros usuários.
+
+## Efí Pay
+
+A Efí oferece aos clientes uma Conta Digital gratuita, com automatização completa dos processos financeiros, redução de custos e controle simplificado das cobranças, sejam elas por Pix, Boleto/Bolix, Carnês, Cartões, Marketplace (split de pagamentos), Links de Pagamento, Carnês ou Assinaturas.
+
+Para conhecer um pouco mais sobre a Efí, acompanhe-nos nas redes sociais:
+
+<p align="left">
+    <a href="https://www.youtube.com/c/sejaefi" target="_blank">
+        <img align="center" src="./assets/img/youtube.svg" alt="" height="30"/>
+    </a>
+    <a href="https://www.facebook.com/sejaefi" target="_blank">
+        <img align="center" src="./assets/img/facebook.svg" alt="" height="30"/>
+    </a>
+    <a href="https://twitter.com/sejaefi" target="_blank">
+        <img align="center" src="./assets/img/twitter.svg" alt="" height="30"/>
+    </a>
+    <a href="https://www.instagram.com/sejaefi" target="_blank">
+        <img align="center" src="./assets/img/instagram.svg" alt="" height="30"/>
+    </a>
+    <a href="https://www.linkedin.com/company/sejaefi" target="_blank">
+        <img align="center" src="./assets/img/linkedin.svg" alt="" height="30"/>
+    </a>    
+</p>
---
+++ assets/js/custom.js
@@ -0,0 +1,120 @@
+/*
+ * Loading
+ */
+//if has .loading on body, add  overflow:hidden on html tag
+var html = document.querySelector('html');
+var body = document.querySelector('body');
+
+$(window).load(function () {
+    body.classList.remove("loading");
+    html.classList.remove("hiddenOverflow");
+});
+
+/*
+ * Random Lottie files de animação
+ */
+function animacaoAleatoria() {
+    var lottieFiles = ['team1.json', 'team2.json']; // possíveis lottie files
+    var index = Math.floor(Math.random() * lottieFiles.length);
+
+    var divAnimacaoHeader = document.getElementById("header-team");
+    var divAnimacaoFooter = document.getElementById("footer-team");
+
+    // Header team
+    var animacaoHeader = document.createElement("lottie-player");
+    animacaoHeader.setAttribute('id', 'lottie-comunidade');
+    animacaoHeader.setAttribute('src', './assets/lottiefiles/header-' + lottieFiles[index]);
+    animacaoHeader.setAttribute('speed', '1');
+    animacaoHeader.setAttribute('loop', '');
+    animacaoHeader.setAttribute('autoplay', '');
+    divAnimacaoHeader.appendChild(animacaoHeader);
+
+    // Footer team
+    var animacaoFooter = document.createElement("lottie-player");
+    animacaoFooter.setAttribute('id', 'lottie-devs');
+    animacaoFooter.setAttribute('src', './assets/lottiefiles/footer-' + lottieFiles[index]);
+    animacaoFooter.setAttribute('speed', '1');
+    animacaoFooter.setAttribute('loop', '');
+    animacaoFooter.setAttribute('autoplay', '');
+    divAnimacaoFooter.appendChild(animacaoFooter);
+}
+
+window.onload = function () {
+    animacaoAleatoria();
+}
+
+
+setInterval(() => {
+    document.getElementById("header-team").innerHTML = "";
+    document.getElementById("footer-team").innerHTML = "";
+    animacaoAleatoria();
+}, 60000);
+
+/**
+ * Verifica se a sessão #estatisticas está visível, para iniciar a contagem dos membros
+ */
+var sessaoEtatisticas = document.querySelector("#estatisticas");
+var exibido = false;
+
+function estaVisivel(el) {
+    const posicoes = el.getBoundingClientRect();
+    const inicio = posicoes.top;
+    const fim = posicoes.bottom;
+    let estaVisivel = false
+
+    if ((inicio >= 0) && (fim <= window.innerHeight)) {
+        estaVisivel = true;
+    }
+
+    return estaVisivel;
+
+}
+
+document.addEventListener('scroll', () => {
+    if (!exibido) {
+        if (estaVisivel(sessaoEtatisticas)) {
+            var membrosComunidade = document.getElementById('membros');
+            var membrosSuporte = document.getElementById('time-suporte');
+            var duracao = 2000;
+
+            // Contador de membros da comunidade
+            for (var i = 1; i <= membrosComunidade.innerHTML; i++) {
+                setTimeout(function (nr) {
+                    membrosComunidade.innerHTML = "+" + nr;
+                }, i * duracao / membrosComunidade.innerHTML, i);
+            }
+
+            // Contador de membros da Consultoria Técnica
+            for (var i = 1; i <= membrosSuporte.innerHTML; i++) {
+                setTimeout(function (nr) {
+                    membrosSuporte.innerHTML = "+" + nr;
+                }, i * duracao / membrosSuporte.innerHTML, i);
+            }
+
+            exibido = true;
+        }
+
+    }
+});
+
+
+/**
+ * Retorna o ano atual
+ */
+new Date().getFullYear();
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+// animações
+$(function () {
+    new WOW().init();
+});
+
+
+/**
+ * Consulta o número de membros da comunidade no Discord
+ */
+fetch('https://discord.com/api/v9/invites/ptGHMtczcV?with_counts=true&with_expiration=false')
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById('membros').innerHTML = data.approximate_member_count;
+    });
